662510008e2429188f281407010c5d399767bcfd
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-21 08:24:20 +0100

    consult data from api and brings full data from each coin

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
 
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: '',
+
+
+}
 const obtenerCriptomonedas = criptomonedas => new Promise( resolve =>{
     resolve(criptomonedas);
 });
@@ -8,6 +17,10 @@ const obtenerCriptomonedas = criptomonedas => new Promise( resolve =>{
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 })
 
 function consultarCriptomonedas(){
@@ -27,6 +40,65 @@ function selectCriptomonedas(criptomonedas){
        option.value = Name;
        option.textContent = FullName;
        criptomonedasSelect.appendChild(option);
+       criptomonedasSelect.appendChild(option);
     })
+
+}
+
+function leerValor(e){
+    objBusqueda[e.target.name] = e.target.value;
+    console.log(objBusqueda);
+}
+
+
+function submitFormulario(e){
+    e.preventDefault();
+
+    const { moneda, criptomoneda } = objBusqueda;
+
+    if(moneda ===''|| criptomoneda === ''){
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+
+    consultarAPI();
+
+}
+
+function mostrarAlerta(msg){
+
+    const existeError = document.querySelector('.error');
+
+    if (!existeError) {
+        const divMensaje = document.createElement('div');
+
+    divMensaje.classList.add('error');
+
+    divMensaje.textContent = msg;
+
+    formulario.appendChild(divMensaje);
+
+    setTimeout(() =>{
+        divMensaje.remove();
+        }, 3000);
+    }  
+}
+
+
+function consultarAPI(){
+    const { moneda, criptomoneda } = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(cotizacion =>{
+        mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+    })
+
+
+    function mostrarCotizacionHTML(cotizacion){
+        console.log(cotizacion);
+    }
 
 }
